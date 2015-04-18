2d3cce5b847f8c2648a241dc0542b39763f79dd2
Suresh Subramaniam <sureshs.592@gmail.com>
2015-04-18 17:54:30 +0800

    Added string representation for model class (logging convenience)

--- app/src/main/java/com/suresh/mapchallenge/api/model/Place.java
+++ app/src/main/java/com/suresh/mapchallenge/api/model/Place.java
@@ -10,4 +10,12 @@ public class Place {
     public String id, name, photoId;
     public ArrayList<String> types;
     public double lat, lng;
+
+    @Override
+    public String toString() {
+        String output = "ID: " + id + ", Name: " + name + ", types: " + types
+                + ", Location: (" + lat + "," + lng + ")";
+        if (photoId != null) output += ", Photo ID: " + photoId;
+        return output;
+    }
 }
