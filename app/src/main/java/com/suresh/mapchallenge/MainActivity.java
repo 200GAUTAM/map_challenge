cd048a6714b9443be63701cd6869d05eb375da3b
Suresh Subramaniam <sureshs.592@gmail.com>
2015-04-18 11:39:34 +0800

    Switching to support map fragment to make it work with the support library (appcompat). Getting the map object once it's loaded.

--- app/src/main/java/com/suresh/mapchallenge/MainActivity.java
+++ app/src/main/java/com/suresh/mapchallenge/MainActivity.java
@@ -3,11 +3,25 @@ package com.suresh.mapchallenge;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFrag);
+        mapFrag.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 }
