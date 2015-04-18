78c2df7e9d7d6fa492e269b31eb26503058af92c
Suresh Subramaniam <sureshs.592@gmail.com>
2015-04-18 13:27:59 +0800

    Initialising map to user's current location

--- app/src/main/java/com/suresh/mapchallenge/MainActivity.java
+++ app/src/main/java/com/suresh/mapchallenge/MainActivity.java
@@ -8,16 +8,23 @@ import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.suresh.mapchallenge.utils.Constants;
 
-public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, Constants {
 
     private GoogleApiClient googleApiClient;
     private GoogleMap map;
+    private Location latestLocation;
+    private boolean mapLocationInitialised = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +62,19 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        tryInitialisingMapLocation();
+    }
+
+    private void tryInitialisingMapLocation() {
+        if (mapLocationInitialised) return; //Map location has already been initialised
+
+        if (map == null || latestLocation == null) return; //We don't have all info yet
+
+        mapLocationInitialised = true;
+
+        LatLng latLng = new LatLng(latestLocation.getLatitude(), latestLocation.getLongitude());
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, MAP_ZOOM_LEVEL);
+        map.animateCamera(update, 1, null);
     }
 
     /*
@@ -63,10 +83,10 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
 
     @Override
     public void onConnected(Bundle bundle) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        latestLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
 
-        if (location != null) {
-            Log.v("test", "Location = (" + location.getLatitude() + ", " + location.getLongitude() + ")");
+        if (latestLocation != null) {
+            tryInitialisingMapLocation();
         } else {
             Log.v("test", "Location is null!");
         }
---
+++ app/src/main/java/com/suresh/mapchallenge/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.suresh.mapchallenge.utils;
+
+/**
+ * Created by suresh on 18/4/15.
+ */
+public interface Constants {
+
+    public static final float MAP_ZOOM_LEVEL = 16F;
+}
